c7a1d76a51f009c4c92895af733f428841ad57f9
MasafumiYanai <zeal15300152@c-league.jp>
2016-11-29 16:05:39 +0900

    ゲームオーバー追加

--- MagicTwins/BaseCrossDx11/GameOver.cpp
+++ MagicTwins/BaseCrossDx11/GameOver.cpp
@@ -11,12 +11,12 @@ namespace basecross
 		App::GetApp()->RegisterTexture(L"GAMEOVERLOGO_TX", strTexture);
 		strTexture = DataDir + L"OverBack.png";
 		App::GetApp()->RegisterTexture(L"GAMEOVERBACK_TX", strTexture);
-		strTexture = DataDir + L"RetryLogo.png";
-		App::GetApp()->RegisterTexture(L"RETRYLOGO_TX", strTexture);
-		strTexture = DataDir + L"StageSelectLogo.png";
-		App::GetApp()->RegisterTexture(L"STAGESELECTLOGO_TX",strTexture);
-		strTexture = DataDir + L"Title.png";
-		App::GetApp()->RegisterTexture(L"TITLE_TX", strTexture);
+		strTexture = DataDir + L"PauseReTryLogo.png";
+		App::GetApp()->RegisterTexture(L"PAUSERETRYLOGO_TX", strTexture);
+		strTexture = DataDir + L"PauseStageSelect.png";
+		App::GetApp()->RegisterTexture(L"PAUSESTAGESELECT_TX",strTexture);
+		strTexture = DataDir + L"PauseTitleLogo.png";
+		App::GetApp()->RegisterTexture(L"PAUSETITLELOGO_TX", strTexture);
 
 	}
 
@@ -94,18 +94,20 @@ namespace basecross
 		Vector2 WindowSize = Vector2((float)App::GetApp()->GetGameWidth(), (float)App::GetApp()->GetGameHeight());
 		PtrTransform->SetPosition(-600, WindowSize.y / 3.5f, 0);
 		PtrTransform->SetRotation(0, 0, 0);
-		PtrTransform->SetScale(500, 150, 1);
+		PtrTransform->SetScale(500, 500, 1);
 
 		//スプライトをつける
 		auto PtrSprite = RetryLogo->AddComponent<PCTSpriteDraw>();
-		PtrSprite->SetTextureResource(L"RETRYLOGO_TX");
+		PtrSprite->SetTextureResource(L"PAUSERETRYLOGO_TX");
 
-		SetSharedGameObject(L"RetryLogo", RetryLogo);
+		SetSharedGameObject(L"PauseReTryLogo", RetryLogo);
 
 		RetryLogo->SetDrawLayer(2);
 
 		//透明度反映
 		RetryLogo->SetAlphaActive(true);
+
+		m_ReTryLogo = RetryLogo;
 	}
 
 	//ステージセレクトロゴ作成
@@ -117,18 +119,20 @@ namespace basecross
 		Vector2 WindowSize = Vector2((float)App::GetApp()->GetGameWidth(), (float)App::GetApp()->GetGameHeight());
 		PtrTransform->SetPosition(-600, WindowSize.y / 9.5f, 0);
 		PtrTransform->SetRotation(0, 0, 0);
-		PtrTransform->SetScale(500, 150, 1);
+		PtrTransform->SetScale(500, 500, 1);
 
 		//スプライトをつける
 		auto PtrSprite = StageSelectLogo->AddComponent<PCTSpriteDraw>();
-		PtrSprite->SetTextureResource(L"STAGESELECTLOGO_TX");
+		PtrSprite->SetTextureResource(L"PAUSESTAGESELECT_TX");
 
-		SetSharedGameObject(L"StageSelectLogo", StageSelectLogo);
+		SetSharedGameObject(L"PauseStageSelect", StageSelectLogo);
 
 		StageSelectLogo->SetDrawLayer(2);
 
 		//透明度反映
 		StageSelectLogo->SetAlphaActive(true);
+
+		m_StageSelectLogo = StageSelectLogo;
 	}
 
 	//タイトルロゴ作成
@@ -138,26 +142,45 @@ namespace basecross
 		TitleLogo->AddComponent<Transform>();
 		auto PtrTransform = TitleLogo->GetComponent<Transform>();
 		Vector2 WindowSize = Vector2((float)App::GetApp()->GetGameWidth(), (float)App::GetApp()->GetGameHeight());
-		PtrTransform->SetPosition(-600,-30, 0);
+		PtrTransform->SetPosition(-600,-80, 0);
 		PtrTransform->SetRotation(0, 0, 0);
-		PtrTransform->SetScale(600, 200, 1);
+		PtrTransform->SetScale(500, 500, 1);
 
 		//スプライトをつける
 		auto PtrSprite = TitleLogo->AddComponent<PCTSpriteDraw>();
-		PtrSprite->SetTextureResource(L"TITLE_TX");
+		PtrSprite->SetTextureResource(L"PAUSETITLELOGO_TX");
 
-		SetSharedGameObject(L"TitleLogo", TitleLogo);
+		SetSharedGameObject(L"PauseTitleLogo", TitleLogo);
 
 		TitleLogo->SetDrawLayer(2);
 
 		//透明度反映
 		TitleLogo->SetAlphaActive(true);
+
+		m_TitleLogo = TitleLogo;
 	}
 
 	void GameOver::SceneChange()
 	{
-		auto ScenePtr = App::GetApp()->GetScene<Scene>();
-		PostEvent(0.0f, GetThis<ObjectInterface>(), ScenePtr, L"GameStage");
+		/*auto ScenePtr = App::GetApp()->GetScene<Scene>();
+		PostEvent(0.0f, GetThis<ObjectInterface>(), ScenePtr, L"GameStage");*/
+
+		if (m_selectnum == 0)
+		{
+			auto ScenePtr = App::GetApp()->GetScene<Scene>();
+			PostEvent(0.0f, GetThis<ObjectInterface>(), ScenePtr, L"GameStage");
+		}
+		if (m_selectnum == 1)
+		{
+			auto ScenePtr = App::GetApp()->GetScene<Scene>();
+			PostEvent(0.0f, GetThis<ObjectInterface>(), ScenePtr, L"StageSelect");
+		}
+		if (m_selectnum == 2)
+		{
+			auto ScenePtr = App::GetApp()->GetScene<Scene>();
+			PostEvent(0.0f, GetThis<ObjectInterface>(), ScenePtr, L"Title");
+		}
+
 	}
 
 	void GameOver::OnCreate()
@@ -181,15 +204,36 @@ namespace basecross
 			//SEマネージャー
 			SetSharedGameObject(L"SEM", AddGameObject<SEManager>());
 
+			auto obj = AddGameObject<GameObject>();
+			//文字列をつける
+			auto PtrString = obj->AddComponent<StringSprite>();
+			PtrString->SetText(L"");
+			PtrString->SetTextRect(Rect2D<float>(16.0f, 16.0f, 640.0f, 480.0f));
+			PtrString->SetFont(L"", 80);
+			PtrString->SetFontColor(Color4(0, 0, 0, 1));
+			obj->SetDrawLayer(10);
+			SetSharedGameObject(L"StringTxt", obj);
+
+			//大きさ調整
+			m_ReTryLogo->GetComponent<Transform>()->SetScale(m_SelectScale);
+			m_StageSelectLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+			m_TitleLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+
 
 		}
 		catch (...) {
 			throw;
 		}
+
 	}
 
 	void GameOver::OnUpdate()
 	{
+		//文字表示
+		auto getobj = GetSharedGameObject<GameObject>(L"StringTxt", false);
+		wstring txt = Util::IntToWStr(m_selectnum);
+		getobj->GetComponent<StringSprite>()->SetText(txt);
+
 		//*テスト用
 		auto key = App::GetApp()->GetInputDevice().GetKeyState();
 		if (key.m_bPressedKeyTbl[VK_SPACE])
@@ -200,6 +244,65 @@ namespace basecross
 
 
 		auto CntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+
+		Vector2 inputXY = Vector2(CntlVec[0].fThumbLX, CntlVec[0].fThumbLY);
+		if (!m_moveFlg)
+		{
+			if (inputXY.y < -0.8f)
+			{
+				m_moveFlg = true;
+				m_selectnum++;
+				if (m_selectnum > 2)
+				{
+					m_selectnum = 0;
+				}
+			}
+			if (inputXY.y > 0.8f)
+			{
+				m_moveFlg = true;
+				m_selectnum--;
+				if (m_selectnum < 0)
+				{
+					m_selectnum = 2;
+				}
+			}
+
+			//変更あったら
+			if (m_moveFlg)
+			{
+				//Vector3 pos;
+				Vector3 scale;
+				switch (m_selectnum)
+				{
+				case 0:
+					m_ReTryLogo->GetComponent<Transform>()->SetScale(m_SelectScale);
+					m_StageSelectLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+					m_TitleLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+
+
+					break;
+				case 1:
+					m_ReTryLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+					m_StageSelectLogo->GetComponent<Transform>()->SetScale(m_SelectScale);
+					m_TitleLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+
+
+					break;
+				case 2:
+					m_ReTryLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+					m_StageSelectLogo->GetComponent<Transform>()->SetScale(m_NotSelectScale);
+					m_TitleLogo->GetComponent<Transform>()->SetScale(m_SelectScale);
+
+					break;
+
+				}
+			}
+		}
+		else if (inputXY.y < 0.1f && inputXY.y > -0.1f)
+		{
+			m_moveFlg = false;
+		}
+
 		if (CntlVec[0].bConnected)
 		{
 			if (CntlVec[0].wPressedButtons & XINPUT_GAMEPAD_B)
--- MagicTwins/BaseCrossDx11/GameOver.h
+++ MagicTwins/BaseCrossDx11/GameOver.h
@@ -5,6 +5,12 @@ namespace basecross
 {
 	class GameOver : public Stage
 	{
+		
+		shared_ptr<GameObject> m_ReTryLogo;
+		shared_ptr<GameObject> m_StageSelectLogo;
+		shared_ptr<GameObject> m_TitleLogo;
+		shared_ptr<GameObject> m_Map;
+
 		//リソースの作成
 		void CreateResourses();
 		//ビューの作成
@@ -19,10 +25,24 @@ namespace basecross
 		void CreateStageSelect();
 		//タイトルの作成
 		void CreateTitle();
-		//
 
 		//シーン遷移
 		void SceneChange();
+
+		int m_selectnum = 0;
+		//操作フラグ
+		bool m_moveFlg = false;
+		bool m_selectMapFlg = false;
+		//選択時X
+		const float m_SelectX = -750;
+		//非選択時X
+		const float m_NotSelectX = -850;
+		
+
+		//選択時Scale
+		const Vector3 m_SelectScale = Vector3(500, 500, 1);
+		const Vector3 m_NotSelectScale = Vector3(400, 400, 1);
+
 	public:
 		GameOver() :Stage() {}
 		virtual void OnCreate()override;
